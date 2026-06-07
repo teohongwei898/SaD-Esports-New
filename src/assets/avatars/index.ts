ee276602e1bcb2463c06ce5b9b10262eb1bfcdf0
Hong Wei <teohongwei898@gmail.com>
2026-06-07 14:49:25 +0800

    Create index.ts

---
+++ src/assets/avatars/index.ts
@@ -0,0 +1,6 @@
+// src/assets/avatars/index.ts
+export { default as welyyPl } from './welyy PL.png';
+export { default as paynePl } from './PAYNE PL.png';
+export { default as zajPl } from './Zaj PL.png';
+export { default as pa1ntPl } from './Pa1nt PL.png';
+export { default as vitaPl } from './VITA PL.png';
